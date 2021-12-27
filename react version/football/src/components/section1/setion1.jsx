e81b52129e1dbd71e4bead79e1a3eae0f7df6250
jayson7 <lexxiijoo70@gmail.com>
2021-12-27 20:19:54 +0100

    football react version

--- react version/football/src/components/section1/setion1.jsx
+++ react version/football/src/components/section1/setion1.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
-
+import "./section1.css";
 export class Setion1 extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <div className="wrapper">
+          <div className="box1">
+            <h1>WE DEAL FOOTBALL</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+              tempora ipsa nihil quo sapiente eius eveniet soluta ratione neque
+              obcaecati, modi quis laborum inventore perspiciatis nesciunt
+              assumenda voluptatem earum consequatur!
+            </p>
+            <div className="btn">
+              <button className="a"> Rankings </button>
+              <button className="a"> Charts </button>
+            </div>
+          </div>
+          <div className="box2"></div>
+        </div>
       </div>
     );
   }
